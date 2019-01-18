c75ccb11527421eec2541ed6622cfadc0878e848
Ian <ianducloux@gmail.com>
2019-01-17 20:33:25 -0600

    Multiplicaciones de consola

---
+++ 03-Bases-Nodejs/app.js
@@ -0,0 +1,50 @@
+const argv = require('yargs')
+    .command('Listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        },
+        crear: {
+            alias: 'c',
+
+        }
+    })
+    .help()
+    .argv;
+
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        console.log('Listar');
+        listarTabla(argv.base, argv.limite)
+        break;
+
+    case 'crear':
+        console.log('crear');
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(e => console.log(e));
+
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+
+
+
+//console.log(process.argv);
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+
+
